5a70c49e059a8172e3bf89d898a06dbc222a44ab
Gurupad Hegde <gurupad93@gmail.com>
2018-10-18 23:41:43 -0400

    First commit of Metrics.py

---
+++ AWS/Metrics.py
@@ -0,0 +1,10 @@
+class Metrics:
+    #Metrics class is used to obtain the current state of queue(total number of messages in the queue) 
+    #and the total number of instances running.
+    
+    def getMessagesInTheQueue():
+    
+
+    def getNumberOfWorkers():
+    
+
