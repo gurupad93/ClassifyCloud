476eb32c5b114ff37e96bcd66b22bb3e5a19bfe7
Sourabh Parvatikar <sourabhparvatikar@gmail.com>
2018-10-19 00:39:37 -0400

    Update Metrics.py

--- AWS/Metrics.py
+++ AWS/Metrics.py
@@ -1,13 +1,13 @@
 class Metrics:
-    #Metrics class is used to obtain the current state of queue(total number of messages in the queue) 
-    #and the total number of instances running.
+    # Metrics class is used to obtain the current state of queue(total number of messages in the queue) 
+    # and the total number of instances running.
     
     def getMessagesInTheQueue():
-    	#get all the messages in the queue
+    	# get all the messages in the queue
 	pass
 
     def getNumberOfWorkers():
-    	#get the numbers of currently running workers
+    	# get the numbers of currently running workers
 	pass
 
 if __name__ == '__main__':
