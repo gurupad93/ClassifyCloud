f6de6b7d90b086d54f2bf0fac349a073896f9c86
sourabhparvatikar <sourabhparvatikar@gmail.com>
2018-10-19 01:14:10 -0400

    added comments

--- AWS/decide_scaling.py
+++ AWS/decide_scaling.py
@@ -1,12 +1,14 @@
 # This class is responsible for creating and
 class DecideScaling(object):
 
-    #
+    # creates specified number of instances
     def create_instance(self, num_instances):
         pass
 
+    # terminates specified number of instances
     def terminate_instance(self, num_instances):
         pass
 
+    # terminate all instances
     def terminate_all_instances(self):
         pass
