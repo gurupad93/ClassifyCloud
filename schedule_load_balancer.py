f43c42789330816b2be0b65f548b0bc40a3aee25
Sourabh Parvatikar <sourabh@Sourabhs-MacBook-Pro.local>
2018-10-18 23:33:20 -0400

    initial commit

---
+++ schedule_load_balancer.py
@@ -0,0 +1,10 @@
+// ScheduleLoadBalancer keeps calling Decidescaling every 5 sedonds to decide the scaling.
+
+
+class ScheduleLoadBalancer{
+    //constructor runs iteratively with a delay of 5 secs
+    //Get Metrics
+    //Send metrics to decide scaling.
+}
+
+
